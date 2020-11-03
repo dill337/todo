df7da22fd9d75bcbc58390f43bbe6dc404935364
dill337 <64544519+dill337@users.noreply.github.com>
2020-11-03 17:05:06 -0600

    First commit Todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mainDiv">
+    <div>
+      <h1 class="title1">To Do List</h1>
+    </div>
+    <md-field class="inputfield">
+      <md-input
+        class="inputText"
+        v-model="currentTodo"
+        @keydown.enter="addTodo()"
+        placeholder="Add a todo"
+      >
+      </md-input>
+    </md-field>
+    <ul class="todos">
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        :class="{ editing: todo == editedTodo }"
+      >
+        <!-- {{ todo.label }} -->
+        <p
+          class="title"
+          contenteditable="true"
+          v-on:keydown.enter="editedTodo($event, todo)"
+          v-on:blur="editedTodo($event, todo)"
+          v-bind:class="{ completed: todo.completed }"
+        >
+          {{ todo.label }}
+        </p>
+        <input
+          type="checkbox"
+          v-on:change="completedTodo(todo)"
+          v-bind:checked="todo.completed"
+        />
+        <button class="deletebutton" @click="removeTodo(todo)">
+          Delete Task
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        editedToDoId: false,
+        completed: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    completedTodo(todo) {
+      todo.completed = !todo.completed;
+    },
+    editToDo(todo) {
+      return "<input>" + todo.label + "</input>";
+      // todo.editedToDoId = true;
+      // placeholder=todo.label
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.title1 {
+  color: red;
+  padding: 50px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.mainDiv {
+  background-color: black;
+}
+.inputfield {
+  left: 50px;
+}
+.inputText {
+  color: gray;
+}
+.completed {
+  text-decoration: line-through;
+}
+.not-completed {
+  color: black;
+}
+.deletebutton {
+  border: none;
+  color: orange;
+  background-color: black;
+  position: relative;
+  left: 100px;
+  cursor: pointer;
+}
+.editing {
+  display: block;
+}
+.todos {
+  color: blue;
+  list-style-type: none;
+}
+div {
+  background-color: black;
+  position: center;
+}
+body {
+  background-color: black;
 }
 </style>
